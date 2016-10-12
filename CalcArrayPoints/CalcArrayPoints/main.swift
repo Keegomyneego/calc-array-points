f114df75a0a7b6ada8d051f53f556bf648c57a7b
Keegan Farley <keegomyneego@live.com>
2016-10-11 18:26:36 -0700

    created project

---
+++ CalcArrayPoints/CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by Keegan Farley on 10/11/16.
+//  Copyright © 2016 Keegan Farley. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
